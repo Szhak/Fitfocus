2cb597b95a9bf8393e474d1e169201db91445864
Szhak <Ahmad040404@mail.ru>
2024-04-04 23:07:30 +0500

    fix

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telebot import types
 import database
 import util
 
-Token = "6609998211:AAG1inKSYLKS4P4pN5Lmc2XuTxaSjOXmypk"  # Token Bot
+Token = "не покажу"  # Token Bot
 bot = telebot.TeleBot(Token)
 
 stopping_message = False
